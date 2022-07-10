c03f47c788281cd11faa4cab8ec98572a8f4d497
ToplaKavica <Nadja.R1999@gmail.com>
2022-07-10 18:31:43 +0200

    Updated script.js

--- src/script.js
+++ src/script.js
@@ -1,79 +1,45 @@
-let now = new Date();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Sunday",
-];
-let today = days[now.getDay()];
-
-const zeroPad = (num, places) => String(num).padStart(places, "0");
-let hour = zeroPad(now.getHours(), 2);
-let minutes = zeroPad(now.getMinutes(), 2);
-
-let currentDay = document.querySelector("#current-day");
-currentDay.innerHTML = today;
-let currentTime = document.querySelector("#current-time");
-currentTime.innerHTML = `${hour}:${minutes}`;
-let city = document.querySelector("h1");
-
-function showTemperature(response) {
-  document.querySelector("h1").innerHTML = response.data.name;
-  temperature.innerHTML = Math.round(response.data.main.temp);
-  document.querySelector("h3").innerHTML = response.data.weather[0].main;
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
+function formatDate(timestamp) {
+  //calculate the date
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
-function searchCity(cityInput) {
-  let apiKey = "a9f48d5f7ecc57bc3785d4771e2e05af";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].main;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000); //sekunde (iz baze podatkov) pretvoriš v minisekunde
 }
 
-function citySearch(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#city-input").value;
-  searchCity(cityInput);
-}
-let form = document.querySelector("#form-search");
-form.addEventListener("submit", citySearch);
+let apiKey = "a9f48d5f7ecc57bc3785d4771e2e05af";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
-let temperature = document.querySelector(".temp");
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
-function searchLocation(position) {
-  let apiKey = "a9f48d5f7ecc57bc3785d4771e2e05af";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(showTemperature);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
-function tempInC(event) {
-  event.preventDefault();
-
-  temperature.innerHTML = "18";
-}
-let celsius = document.querySelector("#c");
-celsius.addEventListener("click", tempInC);
-
-function tempInF(event) {
-  event.preventDefault();
-
-  temperature.innerHTML = "34";
-}
-let fahrenheit = document.querySelector("#f");
-fahrenheit.addEventListener("click", tempInF);
+axios.get(apiUrl).then(displayTemperature);
